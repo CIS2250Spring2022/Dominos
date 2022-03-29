48733bab0e8cf04f4056a48665b3602737c318b8
rob011235 <rgarner011235@gmail.com>
2022-03-29 14:11:58 -0600

    Initial Commit

---
+++ Assets/Scenes/DominoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+// Update is called once per frame    
+  void Update() {  
+    if (Input.GetMouseButtonDown(0)) 
+    {
+        Debug.Log("clicked");  
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
+        RaycastHit hit;  
+        if (Physics.Raycast(ray, out hit)) 
+        {  
+            //Select stage    
+            if (hit.transform.gameObject.tag=="Domino") 
+            {  
+                hit.transform.Rotate(0,0,15);
+            }  
+        }  
+    }  
+  }  
+}
